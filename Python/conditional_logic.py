5d3a06d39265fbe7af5b811a4b14d4281e61dc5c
Jameson <githinjijmaeson@gmail.com>
2020-12-10 20:45:36 +0300

    conditional logic

---
+++ Python/conditional_logic.py
@@ -0,0 +1,13 @@
+#conditional logic
+is_old=True
+is_licensed=True
+
+if is_old and is_licensed:
+  print('you are old enough to drive, and you have a license!')
+else:
+ print('you are not of age!')
+
+ 
+print('okok')
+
+#elif can be multiple in a condition
